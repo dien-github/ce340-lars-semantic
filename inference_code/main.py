dc89c349347a9a21e77a5751cd4983cc55032b38
BeginnerCoder52 <126638696+BeginnerCoder52@users.noreply.github.com>
2025-06-06 14:36:33 +0700

    update the evaluation file sets in inference_code. comment the Plot "plt" in the end lines of metrics due to the unused "matplotlib"

--- inference_code/main.py
+++ inference_code/main.py
@@ -1,30 +1,32 @@
 import os
-from metrics import get_target_from_data, eval_results
-from model import Model
+from metrics import *
+from model import *
 from PIL import Image
 import time
 import numpy as np
 
 if __name__ == "__main__":
-    # ONLY CHANGE 3 LINES
-    data_path = "D:\File_dai_hoc_UIT\Cac_do_an_UIT\LaRS_SemanticSegmentation\lars"  # path to dataset
-    nc = 20  # number of class
-    path_to_model = "./model.tflite"  # path to model
+    #ONLY CHANGE 5 LINES
+    data_path = './sample_test' #path to dataset
+    nc = 10 #number of class
+    path_to_model = "./model.tflite" #path to model
+    model = Model(model_path=path_to_model) 
+    dataset_name = "dataset" #must be one of these names: "lars", "rescuenet", "loveda"
     #
 
     input_size = 320
-    model = Model(model_path=path_to_model)
     model.prepare()
 
-    image_path = data_path + "/images/"
+    image_paths = get_image_paths(data_path, dataset_name)
     total_time = 0.0
-    total_file = len(os.listdir(image_path))
+    total_file = len(image_paths)
     results = []
-    targets = get_target_from_data(data_path)
+    targets = get_target_from_data(data_path, dataset_name, input_size)
 
-    for fi in os.listdir(image_path):
-        img = Image.open(image_path + "/" + fi).resize((input_size, input_size))
-        labels, _ = targets[fi.rsplit(".", 1)[0]]
+    for fi in image_paths:
+        print(fi)
+        img = Image.open(fi).resize((input_size, input_size))
+        labels = targets[os.path.basename(fi).rsplit(".", 1)[0]]
 
         start_time = time.time()
         preds = model.predict(img)
@@ -33,9 +35,21 @@ if __name__ == "__main__":
         total_time += run_time
         results.append((preds, labels))
 
-    FPS = total_file / total_time
+    FPS = total_file/total_time
     print("Average FPS: {:.3f}".format(FPS))
-    normFPS = FPS / 10
-    mp, mr, map50, map, f1 = eval_results(results, nc, input_size)
-    score = 2 * normFPS * f1 / (normFPS + f1)
+    normFPS = FPS/10
+    f1 = eval_semantic_results(results, nc)
+    score = 2*normFPS*f1/(normFPS + f1)
+    print("F1: {:.3f}".format(f1))
     print("Score: {:.3f}".format(score))
+    
+
+
+
+
+
+
+
+
+
+
--- inference_code/model.py
+++ inference_code/model.py
@@ -1,12 +1,9 @@
 import numpy as np
 import tflite_micro_runtime.interpreter as tflite
 
-from nms import non_max_suppression_yolov8
-
-
 class Model(object):
     def __init__(self, model_path):
-        # super.__init__()
+        #super.__init__()
         self.interpreter = tflite.Interpreter(model_path=model_path)
         self.interpreter.allocate_tensors()
 
@@ -46,38 +43,5 @@ class Model(object):
         output_data = self.interpreter.get_tensor(self.output_details[0]["index"])
         results = np.squeeze(output_data).transpose()
 
-        boxes = []
-        for i in range(self.max_box_count):
-            raw_box = results[i]
-            center_x = raw_box[0]
-            center_y = raw_box[1]
-            w = raw_box[2]
-            h = raw_box[3]
-            class_scores = raw_box[self.BOX_COORD_NUM :]
-            for index, score in enumerate(class_scores):
-                if score > self.score_threshold:
-                    boxes.append([center_x, center_y, w, h, score, index])
-
-        clean_boxes = non_max_suppression_yolov8(
-            boxes, self.class_count, self.keypoint_count
-        )
-
-        # return clean_boxes
-        results = []
-
-        for box in clean_boxes:
-            center_x = box[0] * self.input_width
-            center_y = box[1] * self.input_height
-            w = box[2] * self.input_width
-            h = box[3] * self.input_height
-            half_w = w / 2
-            half_h = h / 2
-            left_x = int(center_x - half_w)
-            right_x = int(center_x + half_w)
-            top_y = int(center_y - half_h)
-            bottom_y = int(center_y + half_h)
-            score = box[4]
-            class_index = box[5]
-            results.append([left_x, top_y, right_x, bottom_y, score, class_index])
-
         return results
+
